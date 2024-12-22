a51d0c2ed12eb70503ce0d147c6d480918908027
TyasPrabaswara <23523206@students.uii.ac.id>
2024-12-22 09:58:36 +0700

    v0.1.27

--- ireport.sql
+++ ireport.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 19, 2024 at 03:31 PM
+-- Generation Time: Dec 22, 2024 at 03:42 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -42,7 +42,8 @@ CREATE TABLE `berita` (
 INSERT INTO `berita` (`id_berita`, `judul`, `isi_berita`, `penulis`, `tanggal_publikasi`) VALUES
 (1, 'Minat Warga Jogja Naik Transportasi Umum Rendah, Siro: Saya Kapok Nunggu Lama Pelayanan Payah', 'SuaraJogja.id - Siro (29) warga Wedomartani, Sleman mengaku kini lebih memilih menggunakan kendaraan pribadi untuk beraktivitas ke seputar Jogja ketimbang menggunakan transportasi umum. Ia menetapkan keputusan tersebut lantaran pernah mengalami pengalaman kurang nyaman saat menggunakan Trans Jogja.\r\n\r\n\"Sistem transportasi umum di Jogja tentu sangat kurang ya, pernah aku nyoba pakai Trans Jogja dari Bandara (Adisutjipto) ke Jalan Sudirman, itu lamanya minta ampun. Udah lamaa pelayanannya payah soalnya enggak diarahin gitu. Itu pertama dan terakhir saya naik transportasi umum di Jogja,\" keluh Siro kepada SuaraJogja.\r\n\r\nMenurutnya hal itu yang kemudian membuatnya dan kebanyakan orang memilih untuk membeli kendaraan pribadi. Sebab tak dimungkiri menjadi lebih efisien dan fleksibel dari sisi waktu.', 'Galih Priatmojo | Hiskia Andika Weadcaksana - Suarajogja', '2024-12-19'),
 (2, 'Dishub DIY Berkomitmen Wujudkan Sarana Prasarana Transportasi Umum Ramah Disabilitas\r\n', 'Yogyakarta - Masih banyaknya fasilitas transportasi umum yang belum ramah disabilitas dan berbagai kendala di lapangan menjadi perhatian Dinas Perhubungan DIY. Hal tersebut disampaikan oleh Sekretaris Dinas Perhubungan DIY, Nunik Arzakiyah saat membuka Sosialisasi Urusan Perhubungan dengan tema Layanan Transportasi yang Ramah Disabilitas pada Rabu, 23 Oktober 2024 di Ruang Rapat Kendalisodo Kantor Dinas Perhubungan DIY. \r\n\r\nPertemuan untuk membahas peningkatan aksesibilitas transportasi publik, khususnya layanan Trans Jogja dan Teman Bus ini diselenggarakan oleh Dinas Perhubungan DIY dan melibatkan berbagai pihak, termasuk dari Forum Komunikasi Difabel DIY, Ombudsman, PT. AMI, serta PT. JTT', ' Contributor Dishub - Dishub DIY', '2024-12-19'),
-(3, 'Sembilan Stasiun Kereta Api di Yogyakarta - Solo Sudah Terintegrasi Dengan Moda Transportasi Lain', 'TEMPO.CO, Yogykarta - Sebanyak sembilan stasiun kereta api yang berada di Daerah Operasi atau Daop 6 Yogyakarta sudah terintegrasi dengan moda transportasi lainnya, baik kereta api jarak jauh, kereta bandara, dan bus. Integrasi ini mempermudah mobilitas masyarakat pengguna jasa PT Kereta Api Indonesia (KAI) yang semakin dinamis.\r\n\r\nKrisbiyantoro, Manager Humas Daop 6 Yogyakarta, mengatakan bahwa inovasi yang dilakukan Daop 6 ini sebagai upaya mendukung pembangunan berkelanjutan, selaras dengan tema HUT ke-79 KAI yaitu Safety and Sustainability. \"Daop 6 secara konsisten menciptakan akses ke transportasi umum lainnya secara aman, efisien, tetap menjaga lingkungan, dan mendukung kesejahteraan sosial-ekonomi di berbagai daerah,” kata Krisbiyantoro, Kamis, 26 September 2024.', 'ATENG', '2024-12-19');
+(3, 'Sembilan Stasiun Kereta Api di Yogyakarta - Solo Sudah Terintegrasi Dengan Moda Transportasi Lain', 'TEMPO.CO, Yogykarta - Sebanyak sembilan stasiun kereta api yang berada di Daerah Operasi atau Daop 6 Yogyakarta sudah terintegrasi dengan moda transportasi lainnya, baik kereta api jarak jauh, kereta bandara, dan bus. Integrasi ini mempermudah mobilitas masyarakat pengguna jasa PT Kereta Api Indonesia (KAI) yang semakin dinamis.\r\n\r\nKrisbiyantoro, Manager Humas Daop 6 Yogyakarta, mengatakan bahwa inovasi yang dilakukan Daop 6 ini sebagai upaya mendukung pembangunan berkelanjutan, selaras dengan tema HUT ke-79 KAI yaitu Safety and Sustainability. \"Daop 6 secara konsisten menciptakan akses ke transportasi umum lainnya secara aman, efisien, tetap menjaga lingkungan, dan mendukung kesejahteraan sosial-ekonomi di berbagai daerah,” kata Krisbiyantoro, Kamis, 26 September 2024.', 'ATENG', '2024-12-19'),
+(4, 'Test Berita 1', 'Test konten berita 1', 'admin', '2024-12-21');
 
 -- --------------------------------------------------------
 
@@ -144,6 +145,13 @@ CREATE TABLE `laporan_lokasi` (
   `media_laporan` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `laporan_lokasi`
+--
+
+INSERT INTO `laporan_lokasi` (`id_laporan`, `id_penumpang`, `jenis_keluhan`, `deskripsi_keluhan`, `tanggal_laporan`, `id_lokasi`, `media_laporan`) VALUES
+(1, 8, 'kerusakan', 'das', '2024-12-21', 1, '/opt/lampp/htdocs/iReport_Project/db/../uploads/8a14e1e2-d86c-4cde-80bb-3036daebeec5-1703558289382.jpg');
+
 -- --------------------------------------------------------
 
 --
@@ -167,7 +175,28 @@ CREATE TABLE `laporan_transportasi` (
 INSERT INTO `laporan_transportasi` (`id_laporan`, `id_penumpang`, `jenis_keluhan`, `deskripsi_keluhan`, `tanggal_laporan`, `id_transportasi`, `media_laporan`) VALUES
 (1, 10, 'kerusakan', 'asd', '2024-12-19', 1, '/opt/lampp/htdocs/iReport_Project/db/../uploads/8a14e1e2-d86c-4cde-80bb-3036daebeec5-1703558289382.jpg'),
 (2, 10, 'kerusakan', 'p', '2024-12-20', 1, '/opt/lampp/htdocs/iReport_Project/db/../uploads/8a14e1e2-d86c-4cde-80bb-3036daebeec5-1703558289382.jpg'),
-(3, 10, 'kebersihan', 'balls', '2024-12-19', 1, '/opt/lampp/htdocs/iReport_Project/db/../uploads/8a14e1e2-d86c-4cde-80bb-3036daebeec5-1703558289382.jpg');
+(3, 10, 'kebersihan', 'balls', '2024-12-19', 1, '/opt/lampp/htdocs/iReport_Project/db/../uploads/8a14e1e2-d86c-4cde-80bb-3036daebeec5-1703558289382.jpg'),
+(4, 10, 'kerusakan', 'bwoah', '2024-12-19', 1, '/opt/lampp/htdocs/iReport_Project/db/../uploads/8a14e1e2-d86c-4cde-80bb-3036daebeec5-1703558289382.jpg'),
+(5, 10, 'kerusakan', 'jjjjjjjjjj', '2024-12-19', 1, '/opt/lampp/htdocs/iReport_Project/db/../uploads/8a14e1e2-d86c-4cde-80bb-3036daebeec5-1703558289382.jpg'),
+(6, 10, 'kerusakan', 'asdadadasdasdasasdasasasdadsasd', '2024-12-19', 1, '/opt/lampp/htdocs/iReport_Project/db/../uploads/8a14e1e2-d86c-4cde-80bb-3036daebeec5-1703558289382.jpg'),
+(7, 10, 'kerusakan', 'test new', '2024-12-20', 1, '/opt/lampp/htdocs/iReport_Project/db/../uploads/8a14e1e2-d86c-4cde-80bb-3036daebeec5-1703558289382.jpg'),
+(8, 10, 'kerusakan', 'asd', '2024-12-20', 1, '/opt/lampp/htdocs/iReport_Project/db/../uploads/8a14e1e2-d86c-4cde-80bb-3036daebeec5-1703558289382.jpg'),
+(9, 10, 'kerusakan', 'asd', '2024-12-20', 1, '/opt/lampp/htdocs/iReport_Project/db/../uploads/8a14e1e2-d86c-4cde-80bb-3036daebeec5-1703558289382.jpg'),
+(10, 10, 'kerusakan', 'asd', '2024-12-20', 1, '/opt/lampp/htdocs/iReport_Project/db/../uploads/8a14e1e2-d86c-4cde-80bb-3036daebeec5-1703558289382.jpg'),
+(11, 10, 'kebersihan', 'asd', '2024-12-20', 1, '/opt/lampp/htdocs/iReport_Project/db/../uploads/8a14e1e2-d86c-4cde-80bb-3036daebeec5-1703558289382.jpg'),
+(12, 10, 'kebersihan', 'asd vxc', '2024-12-21', 1, '/opt/lampp/htdocs/iReport_Project/db/../uploads/8a14e1e2-d86c-4cde-80bb-3036daebeec5-1703558289382.jpg'),
+(13, 10, 'kebersihan', 'asd vxc', '2024-12-21', 1, '/opt/lampp/htdocs/iReport_Project/db/../uploads/8a14e1e2-d86c-4cde-80bb-3036daebeec5-1703558289382.jpg'),
+(14, 10, 'kerusakan', 'asd vnb', '2024-12-21', 1, '/opt/lampp/htdocs/iReport_Project/db/../uploads/8a14e1e2-d86c-4cde-80bb-3036daebeec5-1703558289382.jpg'),
+(15, 8, 'kerusakan', 'asd vbnnb', '2024-12-21', 1, '/opt/lampp/htdocs/iReport_Project/db/../uploads/8a14e1e2-d86c-4cde-80bb-3036daebeec5-1703558289382.jpg'),
+(16, 8, 'kerusakan', 'asd nvbvbnvbnvbnnvb', '2024-12-21', 1, '/opt/lampp/htdocs/iReport_Project/db/../uploads/8a14e1e2-d86c-4cde-80bb-3036daebeec5-1703558289382.jpg'),
+(17, 8, 'kerusakan', 'asd ,./fgjh', '2024-12-28', 1, '/opt/lampp/htdocs/iReport_Project/db/../uploads/8a14e1e2-d86c-4cde-80bb-3036daebeec5-1703558289382.jpg'),
+(18, 8, 'kerusakan', 'asd vvnb', '2024-12-21', 1, '/opt/lampp/htdocs/iReport_Project/db/../uploads/8a14e1e2-d86c-4cde-80bb-3036daebeec5-1703558289382.jpg'),
+(19, 8, 'kerusakan', 'asd bvv', '2024-12-21', 1, '/opt/lampp/htdocs/iReport_Project/db/../uploads/8a14e1e2-d86c-4cde-80bb-3036daebeec5-1703558289382.jpg'),
+(20, 8, 'kerusakan', 'abfc', '2024-12-21', 1, '/opt/lampp/htdocs/iReport_Project/db/../uploads/8a14e1e2-d86c-4cde-80bb-3036daebeec5-1703558289382.jpg'),
+(21, 8, 'kerusakan', 'asd vvvv', '2024-12-21', 1, '/opt/lampp/htdocs/iReport_Project/db/../uploads/8a14e1e2-d86c-4cde-80bb-3036daebeec5-1703558289382.jpg'),
+(22, 8, 'kerusakan', 'vvvvvvvvvvvvvvvvvvvvvvvvv', '2024-12-31', 1, '/opt/lampp/htdocs/iReport_Project/db/../uploads/8a14e1e2-d86c-4cde-80bb-3036daebeec5-1703558289382.jpg'),
+(23, 8, 'kebersihan', 'annnnnnnnnnnnnnnnnnnnnn', '2024-12-21', 1, '/opt/lampp/htdocs/iReport_Project/db/../uploads/8a14e1e2-d86c-4cde-80bb-3036daebeec5-1703558289382.jpg'),
+(24, 8, 'kerusakan', 'asdasdasdasdfnmhfhngmg rehga', '2024-12-21', 1, '/opt/lampp/htdocs/iReport_Project/db/../uploads/8a14e1e2-d86c-4cde-80bb-3036daebeec5-1703558289382.jpg');
 
 -- --------------------------------------------------------
 
@@ -196,7 +225,8 @@ INSERT INTO `lokasi` (`id_lokasi`, `nama_lokasi`, `alamat`) VALUES
 
 CREATE TABLE `riwayat_laporan_lokasi` (
   `id_riwayat` int(11) NOT NULL,
-  `id_laporan_lokasi` int(11) NOT NULL,
+  `id_laporan` int(11) NOT NULL,
+  `id_penumpang` int(11) NOT NULL,
   `tanggal_perubahan` date NOT NULL,
   `status` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -209,7 +239,8 @@ CREATE TABLE `riwayat_laporan_lokasi` (
 
 CREATE TABLE `riwayat_laporan_transportasi` (
   `id_riwayat` int(11) NOT NULL,
-  `id_laporan_transportasi` int(11) NOT NULL,
+  `id_laporan` int(11) NOT NULL,
+  `id_penumpang` int(11) NOT NULL,
   `tanggal_perubahan` date NOT NULL,
   `status` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -262,7 +293,8 @@ INSERT INTO `user` (`id_penumpang`, `nama`, `no_telp`, `email`, `username`, `pas
 (7, 'agus rales', 812477132, 'agusrales@gmail.com', 'agusAsli', '$2y$10$DozkbsoXhNmNcutVjvP8du/3wu5PX8DPdIQLycXN5qo7/Wlx.NHXS', 'user'),
 (8, 'test4', 87668754, 'test4@gmail.com', 'test4', '$2y$10$EZEMLG9mSu6fz2DzWclVmubLeOS7lAXOMsNNJCEBVOG6zSH.N/gkG', 'user'),
 (9, 'bola', 18172475, 'bola@gmail.com', 'bola', '$2y$10$ygikqb2wXmqbkrVT9q174utE8lruPQuWje5QKYYLaxK.KDqAvZ9E6', 'user'),
-(10, 'Vickers', 81236427, 'vckrs@gmail.com', 'vckrsmk11', '$2y$10$nBKmIwkKqTpqkn1d9cz0Kehdij7gYv2Bwz.wWkLMq7S2yUcKu79Pq', 'user');
+(10, 'Vickers', 81236427, 'vckrs@gmail.com', 'vckrsmk11', '$2y$10$nBKmIwkKqTpqkn1d9cz0Kehdij7gYv2Bwz.wWkLMq7S2yUcKu79Pq', 'user'),
+(12, 'Ridwan', 81234124, 'ridwan@gmail.com', 'rdwn', '$2y$10$zOZypLjyuqvRq7Rs6L3ixe4WrI5hOklv/YNUBTLQURoJifmtnw7UK', 'admin');
 
 --
 -- Indexes for dumped tables
@@ -293,8 +325,7 @@ ALTER TABLE `laporan_lokasi`
 --
 ALTER TABLE `laporan_transportasi`
   ADD PRIMARY KEY (`id_laporan`),
-  ADD KEY `fk_laporan_user` (`id_penumpang`),
-  ADD KEY `fk_laporan_transportasi` (`id_transportasi`);
+  ADD KEY `fk_laporan_user` (`id_penumpang`);
 
 --
 -- Indexes for table `lokasi`
@@ -307,14 +338,16 @@ ALTER TABLE `lokasi`
 --
 ALTER TABLE `riwayat_laporan_lokasi`
   ADD PRIMARY KEY (`id_riwayat`),
-  ADD KEY `fk_riwayat_lokasi` (`id_laporan_lokasi`);
+  ADD KEY `fk_riwayat_lokasi` (`id_laporan`),
+  ADD KEY `fk_id_penumpang_lokasi` (`id_penumpang`);
 
 --
 -- Indexes for table `riwayat_laporan_transportasi`
 --
 ALTER TABLE `riwayat_laporan_transportasi`
   ADD PRIMARY KEY (`id_riwayat`),
-  ADD KEY `fk_riwayat_transportasi` (`id_laporan_transportasi`);
+  ADD KEY `fk_id_laporan` (`id_laporan`),
+  ADD KEY `fk_id_penumpang` (`id_penumpang`);
 
 --
 -- Indexes for table `transportasi`
@@ -336,7 +369,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `berita`
 --
 ALTER TABLE `berita`
-  MODIFY `id_berita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_berita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `dishub`
@@ -348,13 +381,13 @@ ALTER TABLE `dishub`
 -- AUTO_INCREMENT for table `laporan_lokasi`
 --
 ALTER TABLE `laporan_lokasi`
-  MODIFY `id_laporan` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_laporan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `laporan_transportasi`
 --
 ALTER TABLE `laporan_transportasi`
-  MODIFY `id_laporan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_laporan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `lokasi`
@@ -384,7 +417,7 @@ ALTER TABLE `transportasi`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_penumpang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_penumpang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Constraints for dumped tables
@@ -408,13 +441,15 @@ ALTER TABLE `laporan_transportasi`
 -- Constraints for table `riwayat_laporan_lokasi`
 --
 ALTER TABLE `riwayat_laporan_lokasi`
-  ADD CONSTRAINT `fk_riwayat_lokasi` FOREIGN KEY (`id_laporan_lokasi`) REFERENCES `laporan_lokasi` (`id_laporan`);
+  ADD CONSTRAINT `fk_id_penumpang_lokasi` FOREIGN KEY (`id_penumpang`) REFERENCES `user` (`id_penumpang`),
+  ADD CONSTRAINT `fk_riwayat_lokasi` FOREIGN KEY (`id_laporan`) REFERENCES `laporan_lokasi` (`id_laporan`);
 
 --
 -- Constraints for table `riwayat_laporan_transportasi`
 --
 ALTER TABLE `riwayat_laporan_transportasi`
-  ADD CONSTRAINT `fk_riwayat_transportasi` FOREIGN KEY (`id_laporan_transportasi`) REFERENCES `laporan_transportasi` (`id_laporan`);
+  ADD CONSTRAINT `fk_id_laporan` FOREIGN KEY (`id_laporan`) REFERENCES `laporan_transportasi` (`id_laporan`),
+  ADD CONSTRAINT `fk_id_penumpang` FOREIGN KEY (`id_penumpang`) REFERENCES `user` (`id_penumpang`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
